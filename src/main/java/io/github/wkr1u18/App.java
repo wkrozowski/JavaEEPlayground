4250c04db3a8ba1f70db7fa23adf686e8569242f
wkr1u18 <wkr1u18@soton.ac.uk>
2019-09-05 11:41:52 +0200

    Logging and fat jar configuration with maven-assembly-plugin

---
+++ src/main/java/io/github/wkr1u18/App.java
@@ -0,0 +1,11 @@
+package io.github.wkr1u18;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class App {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello World!");
+        System.out.println("Hello world");
+    }
+}
--- src/test/java/io/github/wkr1u18/AppTest.java
+++ src/test/java/io/github/wkr1u18/AppTest.java
@@ -1,6 +1,8 @@
-import static org.junit.Assert.assertTrue;
+package io.github.wkr1u18;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class AppTest {
     @Test
