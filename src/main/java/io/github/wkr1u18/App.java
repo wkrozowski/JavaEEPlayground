7b989d2dd65c089776f4b3bbd5bf81fa1f1929b9
wkr1u18 <wkr1u18@soton.ac.uk>
2019-09-05 13:20:43 +0200

    Handling http parameters in servlet

--- src/main/java/io/github/wkr1u18/App.java
+++ src/main/java/io/github/wkr1u18/App.java
@@ -1,11 +1,38 @@
 package io.github.wkr1u18;
 
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Logger logger = LoggerFactory.getLogger(App.class);
+
+        var webapp = new WebAppContext();
+        webapp.setResourceBase("src/main/webapp");
+        webapp.setContextPath("/");
+        webapp.setConfigurations(new Configuration[]
+                {
+                        new AnnotationConfiguration(),
+                        new WebInfConfiguration(),
+                        new WebXmlConfiguration(),
+                        new MetaInfConfiguration(),
+                        new FragmentConfiguration(),
+                        new EnvConfiguration(),
+                        new PlusConfiguration(),
+                        new JettyWebXmlConfiguration()
+                });
+        webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
+
+        Server server = new Server(8080);
+        server.setHandler(webapp);
+        server.start();
+        server.join();
         logger.info("Hello World!");
         System.out.println("Hello world");
+
     }
 }
--- src/main/java/io/github/wkr1u18/HelloServlet.java
+++ src/main/java/io/github/wkr1u18/HelloServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 
 @WebServlet(name = "hello", urlPatterns = {"/api/*"})
@@ -15,8 +16,9 @@ public class HelloServlet extends HttpServlet {
     private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        logger.info("Request got");
-        resp.getWriter().write("<h1>Hello world!</h1>");
+        logger.info("Request got with parameters: " + req.getParameterMap());
+        String contents = Optional.ofNullable(req.getParameter("name")).orElse("World");
+        resp.getWriter().write("<h1>Hello " + contents + "!</h1>");
     }
 
 }
